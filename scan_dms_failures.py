dcf97484213ec4452a689a60918e5464ab0bbb86
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-07-22 11:11:17 -0500

    Update scan_dms_failures.py

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,31 +1,39 @@
 SELECT DISTINCT
-    dependent_ns.nspname AS materialized_view_schema,
-    dependent_view.relname AS materialized_view_name,
-    source_ns.nspname AS source_schema,
-    source_object.relname AS source_object_name,
-    CASE source_object.relkind
-        WHEN 'r' THEN 'table'
-        WHEN 'v' THEN 'view'
-        WHEN 'm' THEN 'materialized view'
-        WHEN 'p' THEN 'partitioned table'
-        WHEN 'f' THEN 'foreign table'
-        ELSE source_object.relkind::text
-    END AS source_object_type
+    n.nspname AS dependent_schema,
+    c.relname AS dependent_object,
+    CASE c.relkind
+        WHEN 'v' THEN 'VIEW'
+        WHEN 'm' THEN 'MATERIALIZED VIEW'
+        ELSE c.relkind::text
+    END AS object_type
 FROM pg_depend d
-JOIN pg_rewrite rw
-    ON d.objid = rw.oid
-JOIN pg_class dependent_view
-    ON rw.ev_class = dependent_view.oid
-JOIN pg_namespace dependent_ns
-    ON dependent_view.relnamespace = dependent_ns.oid
-JOIN pg_class source_object
-    ON d.refobjid = source_object.oid
-JOIN pg_namespace source_ns
-    ON source_object.relnamespace = source_ns.oid
-WHERE dependent_view.relkind = 'm'
-  AND dependent_ns.nspname = 'your_schema'
-  AND dependent_view.relname = 'your_materialized_view'
-  AND source_object.oid <> dependent_view.oid
-ORDER BY
-    source_schema,
-    source_object_name;
+JOIN pg_rewrite r
+    ON r.oid = d.objid
+JOIN pg_class c
+    ON c.oid = r.ev_class
+JOIN pg_namespace n
+    ON n.oid = c.relnamespace
+WHERE d.refobjid = 'your_schema.your_materialized_view'::regclass
+  AND c.oid <> d.refobjid
+ORDER BY dependent_schema, dependent_object;
+
+
+
+-- Also check objects that depend on the materialized view's columns
+SELECT DISTINCT
+    n.nspname AS dependent_schema,
+    c.relname AS dependent_object,
+    CASE c.relkind
+        WHEN 'v' THEN 'VIEW'
+        WHEN 'm' THEN 'MATERIALIZED VIEW'
+        ELSE c.relkind::text
+    END AS object_type
+FROM pg_depend d
+JOIN pg_rewrite r
+    ON r.oid = d.objid
+JOIN pg_class c
+    ON c.oid = r.ev_class
+JOIN pg_namespace n
+    ON n.oid = c.relnamespace
+WHERE d.refobjid = 'your_schema.your_materialized_view'::regclass
+ORDER BY dependent_schema, dependent_object;
