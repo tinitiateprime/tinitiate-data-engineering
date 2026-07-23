f59d5592c8fe3621d9de1fb6efe7f6c5bf4cb75a
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-07-23 10:53:24 -0500

    Update scan_dms_failures.py

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -7,33 +7,49 @@ INSERT INTO etl_control.etl_table_config
     enabled,
     load_strategy,
     timestamp_column,
-    created_datetime
+    created_datetime,
+    updated_datetime
 )
 SELECT DISTINCT
     source_schema,
     source_table,
-    source_schema,
-    source_table,
+    target_schema,
+    target_table,
     TRUE,
 
     CASE
         WHEN source_schema IN ('CP','TE')
             THEN 'CURRENT_DAY_MERGE'
+
+        WHEN source_schema IN ('CLM','HRIS','OS')
+            THEN 'SNAPSHOT_REPLACE'
+
         ELSE 'SNAPSHOT_REPLACE'
-    END,
+    END AS load_strategy,
 
     CASE
         WHEN source_schema IN ('CP','TE')
             THEN 'TIME_STAMP'
-        ELSE NULL
-    END,
 
+        ELSE NULL
+    END AS timestamp_column,
+
+    CURRENT_TIMESTAMP,
     CURRENT_TIMESTAMP
+
 FROM etl_control.etl_load_control
-WHERE NOT EXISTS
+WHERE source_schema IN
+(
+    'CP',
+    'CLM',
+    'HRIS',
+    'OS',
+    'TE'
+)
+AND NOT EXISTS
 (
     SELECT 1
-    FROM etl_control.etl_table_config c
-    WHERE c.source_schema = etl_load_control.source_schema
-      AND c.source_table  = etl_load_control.source_table
+    FROM etl_control.etl_table_config cfg
+    WHERE cfg.source_schema = etl_load_control.source_schema
+      AND cfg.source_table  = etl_load_control.source_table
 );
