01a3e3f6add4e120768d935d14e188fde0feb97f
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-17 18:35:10 -0500

    Update pytest command and install dependencies

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,1 +1,5 @@
-python -m pytest -n auto -q --no-cov-on-fail --dist loadscope --cov=main-function/mt-dm-lambda-src --cov=auth-function/mt-dm-lambda-auth-src --cov-report=term-missing main-function/tests auth-function/tests
+py -m pip install pytest-xdist pytest-cov
+
+py -m pytest --version
+
+py -m pytest -n auto -q --no-cov-on-fail --dist loadscope --cov=main-function/mt-dm-lambda-src --cov=auth-function/mt-dm-lambda-auth-src --cov-report=term-missing main-function/tests auth-function/tests
