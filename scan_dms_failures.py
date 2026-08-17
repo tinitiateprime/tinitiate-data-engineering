f7471a2cbb92e03bda38c6d7a61e1af1ec8d231d
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-17 18:46:22 -0500

    Remove pytest commands and add git diff commands

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,5 +1,3 @@
-py -m pip install pytest-xdist pytest-cov
+git diff --name-only origin/main...origin/kp-api-changes
 
-py -m pytest --version
-
-py -m pytest -n auto -q --no-cov-on-fail --dist loadscope --cov=main-function/mt-dm-lambda-src --cov=auth-function/mt-dm-lambda-auth-src --cov-report=term-missing main-function/tests auth-function/tests
+git diff --stat origin/main...origin/kp-api-changes
