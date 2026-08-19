3a48a787565f3a5f7ab5d2ee49fdcbd6c559dcb4
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-19 13:25:56 -0500

    Update print statement for listing module attributes

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,5 +1,1 @@
-py -c "import db.repositories.po_funding_detail_repo as m; print([n for n in dir(m) if not n.startswith('_')])"
-
-
-cd C:\code\mt-dm-gsapdi-lambda-1
-py generate_api_tests.py po_funding_detail --force
+py -c "import db.repositories.po_funding_detail_repo as m; [print(n) for n in dir(m) if not n.startswith('_')]"
