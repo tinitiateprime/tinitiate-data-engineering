04255667fc324653ba1e7113e207ba93982a4401
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-19 13:31:34 -0500

    Update import path for po_funding_detail_service

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,1 +1,2 @@
-py -c "import db.repositories.po_funding_detail_repo as m; [print(n) for n in dir(m) if not n.startswith('_')]"
+cd C:\code\mt-dm-gsapdi-lambda-1\main-function\mt-dm-lambda-src
+py -c "import domain.services.po_funding_detail_service as m; [print(n) for n in dir(m) if not n.startswith('_')]"
