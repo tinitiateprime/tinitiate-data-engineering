7097db05332a6e876dfed80496df7d9af016edf3
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-20 16:24:24 -0500

    Add GL details unit tests and coverage
    Added unit tests for GL details and included coverage reporting.

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,13 +1,10 @@
-cd C:\code\mt-dm-gsapdi-lambda-1\main-function
+git add tests/unit/db/test_gl_details_repo.py
+git add tests/unit/domain/models/test_gl_details.py
+git add tests/unit/domain/services/test_gl_details_service.py
+git add tests/unit/v1/test_gl_details.py
 
-py -m pytest `
-tests\unit\db\test_gl_details_repo.py `
-tests\unit\domain\models\test_gl_details.py `
-tests\unit\domain\services\test_gl_details_service.py `
-tests\unit\v1\test_gl_details.py `
--v `
---cov=db.repositories.gl_details_repo `
---cov=domain.models.gl_details `
---cov=domain.services.gl_details_service `
---cov=v1.handlers.gl_details `
---cov-report=term-missing
+git status
+
+git commit -m "Add GL details unit tests and coverage"
+
+git push origin jay-api-coverage-fix
