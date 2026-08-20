aa803db6aab5c88e64bd01d1398c29618569876d
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-20 16:47:11 -0500

    Update scan_dms_failures.py

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,16 +1,14 @@
-cd C:\code\mt-dm-gsapdi-lambda-1\main-function
+Get-ChildItem tests -Recurse -Filter "*financials_updated*"
 
-py -m pytest tests\unit -v `
-  --cov=v1.handlers.financials_updated `
-  --cov=domain.services.financials_updated_service `
-  --cov=db.repositories.financials_updated_repo `
-  --cov-report=term-missing
-
-
-py -m pytest tests\unit -v -k "financials_updated" `
-  --cov=v1.handlers.financials_updated `
-  --cov=domain.services.financials_updated_service `
-  --cov=db.repositories.financials_updated_repo `
-  --cov-report=term-missing
-
+py -m pytest `
+tests\unit\db\test_financials_updated_repo.py `
+tests\unit\domain\models\test_financials_updated.py `
+tests\unit\domain\services\test_financials_updated_service.py `
+tests\unit\v1\test_financials_updated.py `
+-v `
+--cov=db.repositories.financials_updated_repo `
+--cov=domain.models.financials_updated `
+--cov=domain.services.financials_updated_service `
+--cov=v1.handlers.financials_updated `
+--cov-report=term-missing
 
