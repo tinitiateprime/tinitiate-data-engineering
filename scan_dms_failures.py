387604915b1276ce5f6453c6a85c968abdac818e
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-20 09:38:01 -0500

    Update scan_dms_failures.py with git commands

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,4 +1,18 @@
-py generate_repo_stack.py --config employee_profile_complete_config.json --force
+git status
 
-py generate_api_tests.py employee_profile_complete --force
+git add path\to\employee_profile_complete_repo.py
+git add path\to\employee_profile_complete_service.py
+git add path\to\employee_profile_complete.py
+git add path\to\employee_profile_complete_handler.py
 
+git add employee_profile_complete_config.json
+git add path\to\test_employee_profile_complete_repo.py
+git add path\to\test_employee_profile_complete_service.py
+git add path\to\test_employee_profile_complete.py
+git add path\to\test_employee_profile_complete_handler.py
+
+
+git diff --cached --name-only
+
+git commit -m "Add employee profile complete API"
+git push origin YOUR_BRANCH_NAME
