c7e0b10893edace922a2b3825e1a065f44cf5043
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-21 01:58:40 -0500

    Add multiple unit tests for various services

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,14 +1,19 @@
-git status
+git add tests/unit/db/test_agent_repo.py
+git add tests/unit/db/test_employee_profile_complete_repo.py
+git add tests/unit/db/test_gl_details_repo.py
+git add tests/unit/db/test_po_funding_detail_repo.py
 
-git diff
+git add tests/unit/domain/services/test_agent_service.py
+git add tests/unit/domain/services/test_employee_profile_complete_service.py
+git add tests/unit/domain/services/test_gl_details_service.py
+git add tests/unit/domain/services/test_po_funding_detail_service.py
 
-git add main-function/tests/unit/db/test_project_info_repo.py
-git add main-function/tests/unit/domain/models/test_project_info.py
-git add main-function/tests/unit/domain/services/test_project_info_service.py
-git add main-function/tests/unit/v1/test_project_info.py
+git add tests/unit/v1/test_employee_profile_complete.py
+git add tests/unit/v1/test_gl_details.py
+git add tests/unit/v1/test_po_funding_detail.py
+git add tests/unit/v1/test_project_financial.py
 
-git status
-
-git commit -m "Add unit tests for project info API"
-
-git branch --show-current
+git add tests/unit/db/test_project_info_repo.py
+git add tests/unit/domain/models/test_project_info.py
+git add tests/unit/domain/services/test_project_info_service.py
+git add tests/unit/v1/test_project_info.py
