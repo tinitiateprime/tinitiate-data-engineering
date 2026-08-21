db2682a2cc89659ff73b2b2416f5da15287570d0
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-21 17:09:36 -0500

    Update scan_dms_failures.py

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,15 +1,84 @@
-git ls-files main-function/mt-dm-lambda-src/domain/models/project_status_report.py
-
-git show HEAD:main-function/mt-dm-lambda-src/domain/models/project_status_report.py
-
-
-
-git add main-function/mt-dm-lambda-src/domain/services/project_status_report_service.py
-
-git commit -m "Fix project status report service tests"
-git push origin jay-api-coverage-fix
-
-
-git grep -n "test_lvl_no" -- main-function/tests/unit/domain/services/test_project_status_report_service.py
-
-git add main-function/mt-dm-lambda-src/domain/services/project_status_report_service.py
+{
+  "entity": "ContractModifications",
+  "entity_snake": "contract_modifications",
+  "entity_plural_snake": "contract_modifications",
+  "materialized_view": "gold.contract_modifications_vw",
+  "logical_id_field": "project_id",
+  "lookup_field": "project_id",
+  "default_sort_field": "effective_date",
+  "route_base": "/v1/contracts/modifications",
+  "columns": [
+    {
+      "name": "project_id",
+      "col": "project_id",
+      "type": "text",
+      "alias": "projectId",
+      "required": true,
+      "sortable": true,
+      "selectable": true
+    },
+    {
+      "name": "mod_number",
+      "col": "mod_number",
+      "type": "text",
+      "alias": "modNumber",
+      "required": true,
+      "sortable": true,
+      "selectable": true
+    },
+    {
+      "name": "reason_for_modification",
+      "col": "reason_for_modification",
+      "type": "text",
+      "alias": "reasonForModification",
+      "required": false,
+      "sortable": true,
+      "selectable": true
+    },
+    {
+      "name": "award_status",
+      "col": "award_status",
+      "type": "int",
+      "alias": "awardStatus",
+      "required": false,
+      "sortable": true,
+      "selectable": true
+    },
+    {
+      "name": "effective_date",
+      "col": "effective_date",
+      "type": "timestamptz",
+      "alias": "effectiveDate",
+      "required": false,
+      "sortable": true,
+      "selectable": true
+    },
+    {
+      "name": "negotiated_value",
+      "col": "negotiated_value",
+      "type": "numeric",
+      "alias": "negotiatedValue",
+      "required": false,
+      "sortable": true,
+      "selectable": true
+    },
+    {
+      "name": "exercised_value",
+      "col": "exercised_value",
+      "type": "numeric",
+      "alias": "exercisedValue",
+      "required": false,
+      "sortable": true,
+      "selectable": true
+    },
+    {
+      "name": "funded_amount",
+      "col": "funded_amount",
+      "type": "numeric",
+      "alias": "fundedAmount",
+      "required": false,
+      "sortable": true,
+      "selectable": true
+    }
+  ]
+}
