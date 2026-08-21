9f3968a2c7e07c5d074e27beeefe91282f1d1202
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-21 16:45:36 -0500

    Update scan_dms_failures.py

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,8 +1,10 @@
-git add main-function/mt-dm-lambda-src/domain/models/project_status_report.py
+git ls-files main-function/mt-dm-lambda-src/domain/models/project_status_report.py
 
-git add main-function/tests/unit/db/test_project_status_report_repo.py
-git add main-function/tests/unit/domain/models/test_project_status_report.py
-git add main-function/tests/unit/domain/services/test_project_status_report_service.py
-git add main-function/tests/unit/v1/test_project_status_report.py
+git show HEAD:main-function/mt-dm-lambda-src/domain/models/project_status_report.py
 
-git commit -m "Add project status report model and tests"
+
+
+git add main-function/mt-dm-lambda-src/domain/services/project_status_report_service.py
+
+git commit -m "Fix project status report service tests"
+git push origin jay-api-coverage-fix
