b6c7c8aa6d60cfe12fcae98573ec19f48de70c7f
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-21 10:18:30 -0500

    Update scan_dms_failures.py

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,13 +1,7 @@
-Get-ChildItem .\tests\unit\db\test_project_modifications_repo.py
+git add main-function/tests/unit/db/test_project_modifications_repo.py
+git add main-function/tests/unit/domain/models/test_project_modifications.py
+git add main-function/tests/unit/domain/services/test_project_modifications_service.py
+git add main-function/tests/unit/v1/test_project_modifications.py
 
-py -m pytest .\tests\unit\db\test_project_modifications_repo.py -v --cov=db.repositories.project_modifications_repo --cov-report=term-missing
 
-Get-ChildItem .\tests\unit\db\*project_modifications*
-
-py -m pytest .\tests\unit\domain\models\test_project_modifications.py -v --cov=domain.models.project_modifications --cov-report=term-missing
-
-py -m pytest .\tests\unit\domain\services\test_project_modifications_service.py -v --cov=domain.services.project_modifications_service --cov-report=term-missing
-
-py -m pytest .\tests\unit\v1\test_project_modifications.py -v --cov=v1.handlers.project_modifications --cov-report=term-missing
-
-Get-ChildItem .\tests\unit\db\*project_modifications*
+git commit -m "Add unit tests for project modifications API"
