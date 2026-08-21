56f980b084bdd7631050f758d757067dbb66ca55
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-21 16:47:02 -0500

    Fix formatting issues in scan_dms_failures.py

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -8,3 +8,8 @@ git add main-function/mt-dm-lambda-src/domain/services/project_status_report_ser
 
 git commit -m "Fix project status report service tests"
 git push origin jay-api-coverage-fix
+
+
+git grep -n "test_lvl_no" -- main-function/tests/unit/domain/services/test_project_status_report_service.py
+
+git add main-function/mt-dm-lambda-src/domain/services/project_status_report_service.py
