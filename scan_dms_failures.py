f547824e1a15861e677182eeb3cc762e2cbe9b88
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-22 19:06:43 -0500

    Update scan_dms_failures.py

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,4 +1,6 @@
-py -m pytest main-function\tests\unit\v1\test_employees.py -v --cov=v1.handlers.employees --cov-report=term-missing
+$env:PYTHONPATH="$PWD\main-function\mt-dm-lambda-src"
+
+py -c "import v1.handlers.employees as e; print(e.__file__)"
 
 
 py -m pytest main-function\tests\unit\v1\test_employees.py -v --cov=v1.handlers.employees --cov-report=term-missing
