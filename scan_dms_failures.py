13e6b2aff7303b5f39dec5c4513eccbe7837e2f8
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-08-24 17:08:27 -0500

    Update scan_dms_failures.py

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,21 +1,61 @@
-"first_name": {"col": "first_name", "type": "text"},
-"last_name": {"col": "last_name", "type": "text"},
-"mid_name": {"col": "mid_name", "type": "text"},
+first_name: Optional[str] = Field(
+    default=None,
+    validation_alias="first_name",
+    serialization_alias="firstName",
+)
+
+last_name: Optional[str] = Field(
+    default=None,
+    validation_alias="last_name",
+    serialization_alias="lastName",
+)
+
+mid_name: Optional[str] = Field(
+    default=None,
+    validation_alias="mid_name",
+    serialization_alias="midName",
+)
+
+
+sotv_employee_id: Optional[str] = Field(
+    default=None,
+    validation_alias="sotv_employee_id",
+    serialization_alias="sotvEmployeeId",
+)
+
+first_name: Optional[str] = Field(
+    default=None,
+    validation_alias="first_name",
+    serialization_alias="firstName",
+)
+
+last_name: Optional[str] = Field(
+    default=None,
+    validation_alias="last_name",
+    serialization_alias="lastName",
+)
+
+mid_name: Optional[str] = Field(
+    default=None,
+    validation_alias="mid_name",
+    serialization_alias="midName",
+)
+
+employee_name: Optional[str] = Field(
+    default=None,
+    validation_alias="employee_name",
+    serialization_alias="employeeName",
+)
+"first_name": {"operators": {"eq", "in", "contains"}},
+"last_name": {"operators": {"eq", "in", "contains"}},
+"mid_name": {"operators": {"eq", "in", "contains"}},
 
 
 "first_name",
 "last_name",
 "mid_name",
 
+"firstName": "first_name",
+"lastName": "last_name",
+"midName": "mid_name",
 
-first_name: Optional[str] = Field(None, alias="firstName")
-last_name: Optional[str] = Field(None, alias="lastName")
-mid_name: Optional[str] = Field(None, alias="midName")
-
-
-{
-  "firstName": "...",
-  "lastName": "...",
-  "midName": "...",
-  "employeeName": "..."
-}
