3d6328e039172872dcafcc01a94f2e5e5a249db3
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-09-14 15:35:12 -0500

    Update scan_dms_failures.py

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,18 +1,9 @@
-SELECT DISTINCT
-    n2.nspname AS source_schema,
-    c2.relname AS source_object
-FROM pg_rewrite r
-JOIN pg_class c1
-    ON r.ev_class = c1.oid
-JOIN pg_namespace n1
-    ON c1.relnamespace = n1.oid
-JOIN pg_depend d
-    ON d.objid = r.oid
-JOIN pg_class c2
-    ON d.refobjid = c2.oid
-JOIN pg_namespace n2
-    ON c2.relnamespace = n2.oid
-WHERE n1.nspname = 'gold'
-  AND c1.relname = 'financials_updated_vw'
-  AND c2.relkind IN ('r', 'v', 'm', 'f')
-ORDER BY source_schema, source_object;
+{
+  "Effect": "Allow",
+  "Action": [
+    "s3:GetObject"
+  ],
+  "Resource": [
+    "arn:aws-us-gov:s3:::aws-glue-assets-514899973745-us-gov-west-1/scripts/*"
+  ]
+}
