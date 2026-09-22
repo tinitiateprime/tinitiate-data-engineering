392f0a7b9c443d03ae9697ca5ac5ab6f38aaf43a
jaykumsi <137452836+jaykumsi@users.noreply.github.com>
2026-09-22 12:18:34 -0500

    Update scan_dms_failures.py

--- scan_dms_failures.py
+++ scan_dms_failures.py
@@ -1,19 +1,12 @@
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowEventBridgeToPublish",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "events.amazonaws.com"
-      },
-      "Action": "sns:Publish",
-      "Resource": "arn:aws-us-gov:sns:us-gov-west-1:514899973745:glue-job-mt-dm-glue-clm",
-      "Condition": {
-        "ArnEquals": {
-          "aws:SourceArn": "arn:aws-us-gov:events:us-gov-west-1:514899973745:rule/mt-dm-clm-failure-notify"
-        }
-      }
-    }
-  ]
+  "account": "$.account",
+  "eventTime": "$.time",
+  "jobName": "$.detail.jobName",
+  "jobRunId": "$.detail.jobRunId",
+  "message": "$.detail.message",
+  "region": "$.region",
+  "severity": "$.detail.severity",
+  "state": "$.detail.state"
 }
+
+"AWS GLUE JOB FAILURE ALERT\n\nJob Name: <jobName>\nStatus: <state>\nSeverity: <severity>\nRegion: <region>\nAWS Account: <account>\nJob Run ID: <jobRunId>\nFailure Time (UTC): <eventTime>\n\nError Details:\n<message>\n\nAction Required:\nReview the AWS Glue job run and CloudWatch logs for additional details."
